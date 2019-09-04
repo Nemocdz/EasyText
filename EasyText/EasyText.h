12c6a65315d1f9de75af6587ffdd2b9f4b0845b8
Nemocdz <nemocdz@gmail.com>
2019-09-04 11:28:37 +0800

    fix: header

--- EasyText/EasyText.h
+++ EasyText/EasyText.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 nemocdz. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for EasyText.
 FOUNDATION_EXPORT double EasyTextVersionNumber;
